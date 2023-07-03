b1cb4648a4af936a9c1fe53c2f33df8456e9ed06
JasnoorMaan <jasnoorm0001@gmail.com>
2023-07-03 14:13:25 +0530

    checking the branch

---
+++ Pixel/src/pages/CommitteePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CommitteeCard from '../components/CommitteeCard.jsx'
+
+const CommitteePage = () => {
+  return (
+    <CommitteeCard />
+  )
+}
+
+export default CommitteePage
