b154aecbba8f05831e9189fd4a4149a7d999b04a
JasnoorMaan <jasnoorm0001@gmail.com>
2023-07-03 21:21:45 +0530

    Committee, Awards, Contact pages done...hunn nah chhedho

--- Pixel/src/pages/Home.jsx
+++ Pixel/src/pages/Home.jsx
@@ -4,10 +4,12 @@ import Footer from '../components/Footer.jsx'
 import Announcement from '../components/Announcement.jsx'
 import SponsorHome from '../components/SponsorHome.jsx'
 import Timeline from '../components/Timeline.jsx'
+import Crousal from '../components/Crousal.jsx'
 
 const Home = () => {
   return (
     <>
+    <Crousal />
     <Announcement />
     <SponsorHome />
     <Timeline />
