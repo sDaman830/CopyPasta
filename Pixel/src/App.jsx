c1f57e1e966f3bb831b3ac66eb96cc618d19ac7f
JasnoorMaan <jasnoorm0001@gmail.com>
2023-07-03 14:12:51 +0530

    Checking the branch

--- Pixel/src/App.jsx
+++ Pixel/src/App.jsx
@@ -1,23 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar.jsx'
-import Announcement from './components/Announcement.jsx'
-import Timeline from './components/Timeline.jsx'
-import SponsorHome from './components/SponsorHome.jsx'
 import Footer from './components/Footer.jsx'
-import SpeakerCard from './Components/SpeakerCard'
-import CommitteeCard from './Components/CommitteeCard'
-import SpeakerHeading from './pages/SpeakerHeading'
 import AwardPage from './pages/AwardPage'
 import ContactPage from './pages/ContactPage'
-
-
+import CommitteePage from './pages/CommitteePage.jsx'
 
 function App() {
     return(
         <>
         <Navbar />
-        <ContactPage />
+        <CommitteePage />
         <Footer />
         </>
     )
