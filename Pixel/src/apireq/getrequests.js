81fa8b47f619b9a394d70185e8710bc85b3ca408
sDaman830 <99036076+sDaman830@users.noreply.github.com>
2023-07-05 19:37:53 +0530

    Location thingy

--- Pixel/src/apireq/getrequests.js
+++ Pixel/src/apireq/getrequests.js
@@ -1,57 +1,68 @@
-import {baseUrl,conferencesUrl,apiKey,confId} from '../../api'
+import { baseUrl, conferencesUrl, apiKey, confId } from '../../api'
+
 
 export function getannounce() {
   return fetch(`${baseUrl}/announcement`, {
-      headers: {
-        'Authorization': `${apiKey}`,
-      }
-    }).then(data => data.json())
+    headers: {
+      'Authorization': `${apiKey}`,
+    },
+  }).then(data => data.json());
 }
 
 export function getevents() {
   return fetch(`${baseUrl}/eventDates/conference/${confId}`, {
-      headers: {
-        'Authorization': `${apiKey}`,
-      }
-    }).then(data => data.json())
+    headers: {
+      'Authorization': `${apiKey}`,
+    },
+  }).then(data => data.json());
 }
 
 export function getspeakers() {
   return fetch(`${baseUrl}/speakers`, {
-      headers: {
-        'Authorization': `${apiKey}`,
-      }
-    }).then(data => data.json())
+    headers: {
+      'Authorization': `${apiKey}`,
+    },
+  }).then(data => data.json());
 }
 
 export function getcommittee() {
   return fetch(`${baseUrl}/committees/conference/${confId}`, {
-      headers: {
-        'Authorization': `${apiKey}`,
-      }
-    }).then(data => data.json())
+    headers: {
+      'Authorization': `${apiKey}`,
+    },
+  }).then(data => data.json());
 }
 
 export function getawards() {
   return fetch(`${baseUrl}/awards`, {
-      headers: {
-        'Authorization': `${apiKey}`,
-      }
-    }).then(data => data.json())
+    headers: {
+      'Authorization': `${apiKey}`,
+    },
+  }).then(data => data.json());
 }
 
 export function getsponsors() {
   return fetch(`${baseUrl}/sponsors`, {
-      headers: {
-        'Authorization': `${apiKey}`,
-      }
-    }).then(data => data.json())
+    headers: {
+      'Authorization': `${apiKey}`,
+    },
+  }).then(data => data.json()).then();
 }
 
 export function getcontacts() {
-    return fetch(`${baseUrl}/contacts/${confId}`, {
-        headers: {
-          'Authorization': `${apiKey}`,
-        }
-      }).then(data => data.json())
-  }
+  return fetch(`${baseUrl}/contacts/${confId}`, {
+    headers: {
+      'Authorization': `${apiKey}`,
+    },
+  }).then(data => data.json());
+}
+
+export function getlatitudeandlocation() {
+  return fetch(`${baseUrl}/location/${confId}`, {
+    headers: {
+      'Authorization': `${apiKey}`,
+    },
+  })
+    .then(data => data.json())
+    .then(({ latitude, longitude }) => [latitude, longitude]);
+}
